88e9003053eaecfd0ff6c0994e6a19e7260bd426
bbartling <ben.bartling@gmail.com>
2019-06-06 10:02:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 econProcess_path = os.path.join(dir_path,'econProcess.py')
 econPlots_path = os.path.join(dir_path,'econPlots.py')
+removeFiles_path = os.path.join(dir_path,'removeFiles.py')
 
 
 #cache buster function
@@ -35,9 +36,10 @@ def econ_process():
     #get data into Pandas and save to static
     df = pd.read_csv(filename, index_col='Date', parse_dates=True)
     df.to_csv('static/econRawData.csv')
-    #run .py files to process & plot data
+    #run .py files to process, plot data, and remove uploaded files
     os.system(f'py {econProcess_path}')
     os.system(f'py {econPlots_path}')
+    os.system(f'py {removeFiles_path}')
     #read data.csv in Pandas to show in Pandas to HTML
     df2 = pd.read_csv('static/econDataProcessed.csv', index_col='Date', parse_dates=True)
     resp = add_header(make_response(render_template('table.html', tables=[df2.to_html(classes='data')], titles=df.columns.values)))
@@ -47,3 +49,4 @@ def econ_process():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
